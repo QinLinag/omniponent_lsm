37a17a5cc7c17aa7de4132389b8ded753ceffa6d
qinliang <2874974475@qq.com>
2025-01-29 13:48:02 +0800

    finish the kv

---
+++ kv/Value.go
@@ -0,0 +1,45 @@
+package kv
+
+import (
+	"encoding/json"
+)
+
+// kv
+type Value struct {
+	Key     string
+	Value   []byte
+	Deleted bool
+}
+
+
+func (v *Value) Copy() *Value {
+	return &Value{
+		Key: v.Key,
+		Value: v.Value,
+		Deleted: v.Deleted,
+	}
+}
+
+//反序列化kv对象value
+func Get[T any](v *Value) (T, error) {
+	var value T
+	err := json.Unmarshal(v.Value, &value)
+	return value, err
+}
+
+//将kv对象中的value序列化
+func Convert[T any](value T) ([]byte, error) {
+	return json.Marshal(value)
+}
+
+//将Value的二进制反序列化为Value对象
+func Decode(data []byte) (Value, error) {
+	var value Value
+	err := json.Unmarshal(data, &value)
+	return value, err
+}
+
+//将kv对象序列话为二进制
+func Encode(value Value) ([]byte, error) {
+	return json.Marshal(value)
+}
